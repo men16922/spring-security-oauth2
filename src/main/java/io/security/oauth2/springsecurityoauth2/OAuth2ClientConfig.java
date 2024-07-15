ed444c9cccaa5e8b5bc69d7a6bad0d64be7e2663
men1692 <men16922@gmail.com>
2024-07-15 10:57:14 +0900

    OAuth 로그인 페이지

--- src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ClientConfig.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ClientConfig.java
@@ -1,45 +1,23 @@
 package io.security.oauth2.springsecurityoauth2;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.ClientRegistrations;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
-import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
-import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+@EnableWebSecurity
 public class OAuth2ClientConfig {
 
-    @Bean
-    public ClientRegistrationRepository clientRegistrationRepository() {
-        return new InMemoryClientRegistrationRepository(keycloakClientRegistration());
-    }
-
-    private ClientRegistration keycloakClientRegistration() {
-
-        return ClientRegistrations.fromIssuerLocation("http://localhost:8080/realms/oauth2")
-                .registrationId("keycloak")
-                .clientId("oauth2-client-app")
-                .clientSecret("CsJlVOw2en5hjFaIHeJYAbL3KTJGmKRf")
-                .redirectUri("http://localhost:8081/login/oauth2/code/keycloak")
-                .build();
-
-//        return ClientRegistration.withRegistrationId("keycloak")
-//                .clientId("oauth2-client-app")
-//                .clientSecret("CsJlVOw2en5hjFaIHeJYAbL3KTJGmKRf")
-//                .clientAuthenticationMethod(ClientAuthenticationMethod.BASIC)
-//                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-//                .redirectUriTemplate("{baseUrl}/login/oauth2/code/{registrationId}")
-//                .scope("openid", "profile", "email", "address", "phone")
-//                .authorizationUri("http://localhost:8080/realms/oauth2")
-//                .tokenUri("http://localhost:8080/realms/oauth2/token")
-//                .userInfoUri("http://localhost:8080/realms/oauth2/userinfo")
-//                .userNameAttributeName(IdTokenClaimNames.SUB)
-//                .jwkSetUri("http://localhost:8080/realms/oauth2/certs")
-//                .clientName("Keycloak")
-//                .build();
-    }
+   @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+       http.authorizeRequests(authRequest ->
+               authRequest
+                       .antMatchers("/loginPage")
+                       .permitAll()
+                       .anyRequest().authenticated());
+//       http.oauth2Login(Customizer.withDefaults());
+       http.oauth2Login(oauth2 -> oauth2.loginPage("/loginPage"));
+       return http.build();
+   }
 }
