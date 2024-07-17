5ff099d5dba8025f90e3bfdb2971d2f97356b7e9
최병민/차량편의서비스개발팀 <men1692@hyundai-autoever.com>
2024-07-18 08:53:53 +0900

    OAuth2AuthorizedClient 이해 및 활용

--- src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ClientConfig.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ClientConfig.java
@@ -2,6 +2,7 @@ package io.security.oauth2.springsecurityoauth2;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
@@ -11,21 +12,12 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class OAuth2ClientConfig {
 
-   @Autowired
-   private ClientRegistrationRepository clientRegistrationRepository;
-
    @Bean
    SecurityFilterChain oauth2SecurityFilterChain(HttpSecurity http) throws Exception {
-      http.authorizeRequests((requests) -> requests.antMatchers("/home").permitAll()
+      http.authorizeRequests((requests) -> requests.antMatchers("/home", "/client","/favicon.ico", "/error").permitAll()
               .anyRequest().authenticated());
-      http.oauth2Login(authLogin ->
-              authLogin.authorizationEndpoint(authEndpoint ->
-                      authEndpoint.authorizationRequestResolver(customOAuth2AuthenticationRequestResolver())));
+      http.oauth2Client(Customizer.withDefaults());
       http.logout().logoutSuccessUrl("/home");
       return http.build();
-   }
-
-   private OAuth2AuthorizationRequestResolver customOAuth2AuthenticationRequestResolver() {
-      return new CustomOAuth2AuthorizationRequestResolver(clientRegistrationRepository, "/oauth2/authorization");
    }
 }
