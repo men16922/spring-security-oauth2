524c35c1e64c3defa7c8dc3dd3b07b469ea99370
men1692 <men16922@gmail.com>
2024-07-17 22:34:58 +0900

    OAuth2AuthorizationRequestResolver

--- src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ClientConfig.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ClientConfig.java
@@ -1,23 +1,32 @@
 package io.security.oauth2.springsecurityoauth2;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 public class OAuth2ClientConfig {
 
+   @Autowired
+   private ClientRegistrationRepository clientRegistrationRepository;
+
    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-       http.authorizeRequests(authRequest ->
-               authRequest
-                       .antMatchers("/loginPage")
-                       .permitAll()
-                       .anyRequest().authenticated());
-       http.oauth2Login(Customizer.withDefaults());
-//       http.oauth2Login(oauth2 -> oauth2.loginPage("/loginPage"));
-       return http.build();
+   SecurityFilterChain oauth2SecurityFilterChain(HttpSecurity http) throws Exception {
+      http.authorizeRequests((requests) -> requests.antMatchers("/home").permitAll()
+              .anyRequest().authenticated());
+      http.oauth2Login(authLogin ->
+              authLogin.authorizationEndpoint(authEndpoint ->
+                      authEndpoint.authorizationRequestResolver(customOAuth2AuthenticationRequestResolver())));
+      http.logout().logoutSuccessUrl("/home");
+      return http.build();
+   }
+
+   private OAuth2AuthorizationRequestResolver customOAuth2AuthenticationRequestResolver() {
+      return new CustomOAuth2AuthorizationRequestResolver(clientRegistrationRepository, "/oauth2/authorization");
    }
 }
