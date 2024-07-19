a5cedac8a82d874f3bbf27c820b681add77d518a
men1692 <men16922@gmail.com>
2024-07-19 17:23:55 +0900

    DefaultOAuth2AuthorizedClientManager - Refresh Token 권한 부여 구현하기

--- src/main/java/io/security/oauth2/springsecurityoauth2/LoginController.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/LoginController.java
@@ -1,4 +1,4 @@
-package io.security.oauth2.springsecurityoauth2;
+package io.security.oauth2.springsecurityoauth2.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -6,8 +6,11 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.OAuth2AuthorizationSuccessHandler;
 import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.OAuth2Token;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +18,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.time.Clock;
+import java.time.Duration;
 
 @Controller
 public class LoginController {
@@ -24,6 +29,10 @@ public class LoginController {
 
     @Autowired
     private OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository;
+
+    private Duration clockSkew = Duration.ofSeconds(3600);
+
+    private Clock clock = Clock.systemUTC();
 
     @GetMapping("/oauth2Login")
     public String oauth2Login(Model model, HttpServletRequest request, HttpServletResponse response) {
@@ -51,10 +60,43 @@ public class LoginController {
 
         OAuth2AuthorizedClient authorizedClient = oAuth2AuthorizedClientManager.authorize(authorizeRequest);
 
-        model.addAttribute("authorizedClient", authorizedClient.getAccessToken().getTokenValue());
+        // 권한 부여 타입을 변경하지 않고 토큰 재발금
+        if (authorizedClient != null && hasTokenExpired(authorizedClient.getAccessToken())
+                && authorizedClient.getRefreshToken() != null) {
+            authorizedClient = oAuth2AuthorizedClientManager.authorize(authorizeRequest);
+        }
+
+        // 권한 부여 타입을 변경하고 토큰 재발급
+        /*if (authorizedClient != null && hasTokenExpired(authorizedClient.getAccessToken())
+                && authorizedClient.getRefreshToken() != null) {
+
+            ClientRegistration clientRegistration = ClientRegistration.withClientRegistration
+                    (authorizedClient.getClientRegistration()).authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                    .build();
+
+            OAuth2AuthorizedClient oAuth2AuthorizedClient =
+                    new OAuth2AuthorizedClient(clientRegistration, authorizedClient.getPrincipalName(),
+                            authorizedClient.getAccessToken(),authorizedClient.getRefreshToken());
+
+            OAuth2AuthorizeRequest oAuth2AuthorizeRequest =
+                    OAuth2AuthorizeRequest.withAuthorizedClient(oAuth2AuthorizedClient)
+                            .principal(authentication)
+                            .attribute(HttpServletRequest.class.getName(), request)
+                            .attribute(HttpServletResponse.class.getName(), response)
+                            .build();
+
+            authorizedClient = oAuth2AuthorizedClientManager.authorize(oAuth2AuthorizeRequest);
+        }*/
+
+        model.addAttribute("AccessToken", authorizedClient.getAccessToken().getTokenValue());
+        model.addAttribute("RefreshToken", authorizedClient.getRefreshToken().getTokenValue());
 
         return "home";
 
+    }
+
+    private boolean hasTokenExpired(OAuth2Token token) {
+        return this.clock.instant().isAfter(token.getExpiresAt().minus(this.clockSkew));
     }
 
 
