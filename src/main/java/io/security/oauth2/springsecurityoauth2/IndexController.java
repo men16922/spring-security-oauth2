a78e8255a0d4c80f26809524de321b66c061ae84
men1692 <men16922@gmail.com>
2024-07-19 13:59:45 +0900

    DefaultOAuth2AuthorizedClientManager - Client Credentials 권한 부여 구현하기

--- src/main/java/io/security/oauth2/springsecurityoauth2/IndexController.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/IndexController.java
@@ -1,19 +1,12 @@
 package io.security.oauth2.springsecurityoauth2;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
-
-    @Autowired
-    private ClientRegistrationRepository clientRegistrationRepository;
-
     @GetMapping("/")
     public String index(){
         return "index";
     }
-
 }
--- src/main/java/io/security/oauth2/springsecurityoauth2/LoginController.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/LoginController.java
@@ -1,6 +1,13 @@
 package io.security.oauth2.springsecurityoauth2;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizationSuccessHandler;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,13 +19,47 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 public class LoginController {
 
+    @Autowired
+    private DefaultOAuth2AuthorizedClientManager oAuth2AuthorizedClientManager;
+
+    @Autowired
+    private OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository;
+
     @GetMapping("/oauth2Login")
     public String oauth2Login(Model model, HttpServletRequest request, HttpServletResponse response) {
-        return "redirect:/";
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        OAuth2AuthorizeRequest authorizeRequest = OAuth2AuthorizeRequest
+                .withClientRegistrationId("keycloak")
+                .principal(authentication)
+                .attribute(HttpServletRequest.class.getName(), request)
+                .attribute(HttpServletResponse.class.getName(), response)
+                .build();
+
+        OAuth2AuthorizationSuccessHandler successHandler = (authorizedClient, principal, attributes) -> {
+            oAuth2AuthorizedClientRepository
+                    .saveAuthorizedClient(authorizedClient, principal,
+                            (HttpServletRequest) attributes.get(HttpServletRequest.class.getName()),
+                            (HttpServletResponse) attributes.get(HttpServletResponse.class.getName()));
+            System.out.println("authorizedClient = " + authorizedClient);
+            System.out.println("principal = " + principal);
+            System.out.println("attributes = " + attributes);
+        };
+
+        oAuth2AuthorizedClientManager.setAuthorizationSuccessHandler(successHandler);
+
+        OAuth2AuthorizedClient authorizedClient = oAuth2AuthorizedClientManager.authorize(authorizeRequest);
+
+        model.addAttribute("authorizedClient", authorizedClient.getAccessToken().getTokenValue());
+
+        return "home";
+
     }
 
+
     @GetMapping("/logout")
-    public String logout(Authentication authentication, HttpServletRequest request, HttpServletResponse response) {
+    public String logout(Authentication authentication, HttpServletResponse response, HttpServletRequest request) {
         SecurityContextLogoutHandler logoutHandler = new SecurityContextLogoutHandler();
         logoutHandler.logout(request, response, authentication);
 
