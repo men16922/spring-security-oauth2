fcd0fffe9870b4c2de63c31b62b56e9a9ce213b5
men1692 <men16922@gmail.com>
2024-07-26 23:43:57 +0900

    JwtDecoder

--- src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ResourceServerConfig.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ResourceServerConfig.java
@@ -1,18 +1,43 @@
 package io.security.oauth2.springsecurityoauth2;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class OAuth2ResourceServerConfig {
+
+    @Autowired
+    private OAuth2ResourceServerProperties oAuth2ResourceServerProperties;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests(request -> request.anyRequest().authenticated());
         http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
         return http.build();
+    }
+
+//    @Bean
+//    public JwtDecoder jwtDecoder1() {
+//        return JwtDecoders.fromIssuerLocation(oAuth2ResourceServerProperties.getJwt().getIssuerUri());
+//    }
+
+//    @Bean
+//    public JwtDecoder jwtDecoder2() {
+//        return JwtDecoders.fromOidcIssuerLocation(oAuth2ResourceServerProperties.getJwt().getIssuerUri());
+//    }
+
+    @Bean
+    public JwtDecoder jwtDecoder3() {
+        return NimbusJwtDecoder.withJwkSetUri(oAuth2ResourceServerProperties.getJwt().getJwkSetUri())
+                .jwsAlgorithm(SignatureAlgorithm.RS512).build();
     }
 }
