f4fd3be1d51e4441e386059b42345d859cf1a36c
men1692 <men16922@gmail.com>
2024-07-28 05:08:34 +0900

    Authentication & @AuthenticationPrincipal 참조하기

--- src/main/java/io/security/oauth2/springsecurityoauth2/controller/IndexController.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/controller/IndexController.java
@@ -1,8 +1,21 @@
 package io.security.oauth2.springsecurityoauth2.controller;
 
+
+import org.springframework.http.HttpMethod;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import org.springframework.http.HttpHeaders;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @RestController
 public class IndexController {
@@ -13,7 +26,23 @@ public class IndexController {
     }
 
     @GetMapping("/api/user")
-    public Authentication user(Authentication authentication) {
+    public Authentication user(Authentication authentication, @AuthenticationPrincipal Jwt principal) throws URISyntaxException {
+
+        JwtAuthenticationToken authenticationToken = (JwtAuthenticationToken) authentication;
+        String sub = (String) authenticationToken.getTokenAttributes().get("sub");
+        String email = (String) authenticationToken.getTokenAttributes().get("email");
+        String scope = (String) authenticationToken.getTokenAttributes().get("scope");
+
+        String sub1 = principal.getClaim("sub");
+        String token = principal.getTokenValue();
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + token);
+        RequestEntity<String> request = new RequestEntity<>(headers, HttpMethod.GET, new URI("http://localhost:9090/user"));
+        ResponseEntity<String> response = restTemplate.exchange(request, String.class);
+        String body = response.getBody();
+
         return authentication;
     }
 }
