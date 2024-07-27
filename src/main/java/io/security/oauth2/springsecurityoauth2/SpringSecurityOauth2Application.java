ba534eba40d1ff8674272ce89fe13f22f22470a2
men1692 <men16922@gmail.com>
2024-07-27 14:49:44 +0900

    JWT - JWK 이해 및 활용하기

--- src/main/java/io/security/oauth2/springsecurityoauth2/SpringSecurityOauth2Application.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/SpringSecurityOauth2Application.java
@@ -3,19 +3,13 @@ package io.security.oauth2.springsecurityoauth2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import static io.security.oauth2.springsecurityoauth2.MacTest.hmac;
-import static io.security.oauth2.springsecurityoauth2.MessageDigestTest.messageDigest;
-import static io.security.oauth2.springsecurityoauth2.RSATest.rsa;
-import static io.security.oauth2.springsecurityoauth2.SignatureTest.signature;
+import static io.security.oauth2.springsecurityoauth2.JWKTest.jwk;
 
 @SpringBootApplication
 public class SpringSecurityOauth2Application {
 
     public static void main(String[] args) throws Exception {
-//        messageDigest("Spring Security");
-//        signature("Spring Security");
-        hmac("Spring Security");
-        rsa("Spring Security");
+        jwk();
         SpringApplication.run(SpringSecurityOauth2Application.class, args);
     }
 
