2efedfca5950d6e27dae0345406a8a8a87d8dbd5
men1692 <men16922@gmail.com>
2024-07-28 18:06:15 +0900

    OAuth 2.0 Client + OAuth 2.0 Resource Server 연동

---
+++ ResourceServer/src/main/java/io/oauth2/resource/resourceserver/OAuth2ResourceServer.java
@@ -0,0 +1,40 @@
+package io.oauth2.resource.resourceserver;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration//(proxyBeanMethods = false)
+public class OAuth2ResourceServer {
+
+
+    @Bean
+    SecurityFilterChain securityFilterChain1(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests(
+                (requests) -> requests
+                        .antMatchers("/photos").access("hasAuthority('SCOPE_photo')")
+                        .anyRequest().authenticated());
+        http.oauth2ResourceServer().jwt();
+        http.cors().configurationSource(corsConfigurationSource());
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.setMaxAge(3600L);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+
+    }
+}
---
+++ ResourceServer/src/test/java/io/oauth2/resource/resourceserver/ResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.oauth2.resource.resourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
